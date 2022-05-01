1236a9b27a095d4e05ea97ed090462c327c26e74
sudharsan2000 <ssudharsan2000@gmail.com>
2022-05-01 19:28:29 +0530

    Move im read

--- code/application/general/preproc.py
+++ code/application/general/preproc.py
@@ -1,7 +1,23 @@
 import numpy as np
 import cv2
+import os 
 
-def preproc(img, input_size, swap=(2, 0, 1)):
+def preproc(img_path, input_size, swap=(2, 0, 1)):
+
+    img_info = {"id": 0}
+    if isinstance(img_path, str):
+            img_info["file_name"] = os.path.basename(img)
+            img = cv2.imread(img)
+    else:
+            img_info["file_name"] = None
+
+    height, width = img.shape[:2]
+    img_info["height"] = height
+    img_info["width"] = width
+    img_info["raw_img"] = img
+
+    ratio = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
+    img_info["ratio"] = ratio
     if len(img.shape) == 3:
         padded_img = np.ones((input_size[0], input_size[1], 3), dtype=np.uint8) * 114
     else:
@@ -18,4 +34,19 @@ def preproc(img, input_size, swap=(2, 0, 1)):
     padded_img = padded_img.transpose(swap)
     padded_img = np.ascontiguousarray(padded_img, dtype=np.float32)
     padded_img = np.expand_dims(padded_img, axis=0)
-    return padded_img, r
+
+    img_info = {"id": 0}
+    if isinstance(img_path, str):
+            img_info["file_name"] = os.path.basename(img)
+    else:
+            img_info["file_name"] = None
+
+    height, width = img.shape[:2]
+    img_info["height"] = height
+    img_info["width"] = width
+    img_info["raw_img"] = img
+
+    ratio = min(input_size[0] / img.shape[0], input_size[1] / img.shape[1])
+    img_info["ratio"] = ratio
+
+    return img, padded_img, img_info, r
